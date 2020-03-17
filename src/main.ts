3a3e0c781e58cced8f93d92b11c8cbb8f0dee3d8
Ashutosh Varma <ashutoshvarma11@live.com>
2020-03-17 16:37:15 +0530

    add complete cmake action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,88 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as io from '@actions/io'
+import * as exec from '@actions/exec'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const buildType: string = core.getInput('build-type') || 'Release'
+    const installBuild: string = core.getInput('install-build')
+    const runTest: string = core.getInput('run-test')
+    const cc: string = core.getInput('cc') || 'gcc'
+    const cxx: string = core.getInput('cxx') || 'g++'
+    const target: string = core.getInput('target') || 'all'
+    const parallel: string = core.getInput('parallel') || '4'
+    const options: string[] = core.getInput('options').split(' ')
+    const ctestOptions: string[] = core.getInput('ctest-options').split(' ')
+    let buildDir: string = core.getInput('build-dir') || 'build'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // setup build directory
+    buildDir = path.join(__dirname, buildDir)
+    await io.mkdirP(buildDir)
 
-    core.setOutput('time', new Date().toTimeString())
+    //export CC & CXX
+    core.exportVariable('CC', cc)
+    core.exportVariable('CXX', cxx)
+
+    //fix path by removing sh.exe
+    core.startGroup('Fixing Path')
+    core.exportVariable('PATH', await fixPath(<string>process.env.PATH))
+    core.endGroup()
+
+    //configure options
+    const configOptions = [
+      `-DCMAKE_BUILD_TYPE=${buildType}`,
+      `-H${__dirname}`,
+      `-B${buildDir}`,
+      ...options
+    ]
+    const buildOptions = [
+      '--build',
+      buildDir,
+      '--config',
+      buildType,
+      '--target',
+      target,
+      `-j${parallel}`
+    ]
+
+    //Configure CMake
+    core.startGroup('Configure CMake')
+    await exec.exec('cmake', configOptions)
+    core.endGroup()
+
+    //Build CMake Project
+    core.startGroup(`Build Target - ${target}`)
+    await exec.exec('cmake', buildOptions)
+    core.endGroup()
+
+    // Install Targets
+    if (installBuild !== '') {
+      core.startGroup(`Installing Build`)
+      await exec.exec('cmake', ['--install'])
+      core.endGroup()
+    }
+
+    // Run Ctest
+    const sourceDir: string = process.cwd()
+    if (runTest !== '') {
+      process.chdir(buildDir)
+      core.startGroup('Running CTest')
+      await exec.exec('ctest', [`-j${parallel}`, ...ctestOptions])
+      core.endGroup()
+      process.chdir(sourceDir)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+export async function fixPath(oldPath: string): Promise<string> {
+  const shDir: string = await io.which('sh.exe')
+  if (shDir !== '') {
+    oldPath = oldPath.replace(path.dirname(shDir), 'DUMMY_PATH')
+  }
+  return oldPath
 }
 
 run()

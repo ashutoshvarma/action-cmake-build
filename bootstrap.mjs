f96bbb425045420bdded2f63b9f1eb805c67f9ad
Ashutosh Varma <ashutoshvarma11@live.com>
2020-03-24 17:26:28 +0530

    rewrite bootstrap script to remove `env` dependency

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,29 +1,46 @@
 /* eslint-disable no-console */
 import fs from 'fs'
 import YAML from 'yaml'
-import {exec} from 'child_process'
+import * as cp from 'child_process'
+import * as path from 'path'
+// import * as io from '@actions/io'
+
 
 const ACTION_FILE = fs.readFileSync('action.yml', 'utf8')
 
-const parseObj = YAML.parse(ACTION_FILE)
-// console.log(parseObj)
-const inputs = parseObj.inputs
+const actionFile = path.join('action.yml')
+const srcDir = path.join('test', 'DemoProject')
+const buildDir = path.join('test', 'build')
 
-let envString = ''
-
-for (const key of Object.keys(inputs)) {
-  if (inputs[key].default) {
-    envString = `${envString} INPUT_${key.replace(/ /g, '_').toUpperCase()}='${
-      inputs[key].default
-    }'`
+function setupInputs() {
+  const parseObj = YAML.parse(fs.readFileSync(actionFile, 'utf-8'))
+  const inputs = parseObj.inputs
+  for (const key of Object.keys(inputs)) {
+    if (inputs[key].default) {
+      const inputEnv = `INPUT_${key.replace(/ /g, '_').toUpperCase()}`
+      process.env[inputEnv] = inputs[key].default
+      console.log(`${inputEnv}=${inputs[key].default}`)
+    }
   }
 }
 
-exec(`env ${envString} node lib/main.js`, (err, stdout, stderr) => {
-  if (err) {
-    console.log(err.message)
+// shows how the runner will run a javascript action with env / stdout protocol
+
+setupInputs()
+// Clean Build dir
+// io.mkdirP(buildDir)
+// cd into source dir
+process.env['INPUT_BUILD-DIR'] = buildDir
+process.env['INPUT_SOURCE-DIR'] = srcDir
+const ip = path.join('lib', 'main.js')
+const options = {
+  env: process.env,
+  stdio: 'inherit'
+}
+const spawnProc = cp.spawn('node', [ip], options)
+spawnProc.on('close', code => {
+  if (code !== 0) {
+    throw Error(`Action exited with code ${code}`)
   }
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout:-\n ${stdout}`)
-  console.log(`stderr:-\n ${stderr}`)
 })
+
